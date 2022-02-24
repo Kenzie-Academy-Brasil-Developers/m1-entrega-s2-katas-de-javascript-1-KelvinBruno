c822cd6a1d8b1f42f828f07f6027278690b581a8
Kelvin Lopes <kelvinbrunolopes@gmail.com>
2022-02-24 13:07:06 -0300

    arrumado

--- katas1.js
+++ katas1.js
@@ -10,7 +10,7 @@ function oneThroughTwenty() {
 function evensToTwenty() {
   let pares = [];
   for (let i = 1; i <= 20; i++) {
-    If(i % 2 == 0);
+    if(i % 2 == 0);
     {
       pares.push(i);
     }
